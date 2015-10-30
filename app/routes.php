829ef576203e1ba7281f50fe9908d84638344ca4
haksho <adrien-richard@live.fr>
2015-10-30 16:24:53 +0100

    liste des annonces twig

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,8 @@
 <?php
 
 $app->get('/', function () use ($app) {
-	/*
-	$villes=ville::all();
-	foreach ($villes as $key => $ville) {
-		echo ($ville->nom);
-	}
-	*/
-	$villes = Ville::all();
-  $app->render('accueil.twig');
+	$annonces = Annonce::all();
+	$app->render('accueil.twig', array('annonces'=> $annonces));
 });
 
 ?>
