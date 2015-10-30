b262173618f019ff8c2a1854f356388908b8b130
haksho <adrien-richard@live.fr>
2015-10-30 17:07:11 +0100

    Se balader dans les fichier twig ( routes)

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,27 @@
 
 $app->get('/', function () use ($app) {
 	$annonces = Annonce::all();
-	$app->render('accueil.twig', array('annonces'=> $annonces));
-});
+	$urlD = $app->urlFor('depot');
+	$urlRech = $app->urlFor('recherche');
+	$app->render('accueil.twig', array('
+		annonces'=> $annonces,
+		'url' =>$urlD,
+		'urlRech' =>$urlRech
+	));
+})->name('accueil');
+
+$app->get('/depotAnnonce' , function () use ($app) {
+	
+	$nouvA = new Annonce();
+
+	$nouvA->save();
+	$app->render('depotAnnonce.twig');
+})->name('depot');
+
+
+$app->get('/recherche' , function () use ($app) {
+
+	$app->render('recherche.twig');
+})->name('recherche');
 
 ?>
